b59d42c6310867dba50f376fcef970801f7e2c13
Sango Donald Ahldaïr Oussoudoma <111059139+Oussoudomasango@users.noreply.github.com>
2022-09-22 09:05:15 +0000

    Add files via upload

---
+++ week_4/Day_1/Exercice_xp_gold.py
@@ -0,0 +1,22 @@
+# Execice 1
+
+print("Hello World\n"*4 + "I love Python\n"*4);
+
+# Exercice 2
+
+Mois = input("Please put a month :  ");
+Mois = str(Mois)
+if(Mois=='March' or Mois=='April' or Mois=='May'):
+    print("We are in Spring")
+elif (Mois=='June' or Mois=='July' or Mois=='August'):
+    print("We are in Summer")
+elif (Mois=='September' or Mois=='October' or Mois=='November'):
+    print("We are in Autumn")
+elif (Mois=='December' or Mois=='January' or Mois=='February'):
+    print("We are in Winter")
+else:
+    print("Try again, Errore value!!!!!!!!!!!!!!")
+    
+    
+    
+    
