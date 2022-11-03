f1c4ed3b7c43ab220126ec4664c7efd3ad5330b4
Sango Donald Ahldaïr Oussoudoma <111059139+Oussoudomasango@users.noreply.github.com>
2022-11-03 09:42:27 +0000

    Add files via upload

---
+++ week_6/day_1/Exercice_xp_gold/Exercice_xp_gold.sql
@@ -0,0 +1,12 @@
+--1
+SELECT first_name,last_name,birth_date FROM students ORDER BY last_name LIMIT 4;
+--2
+SELECT * FROM  students ORDER BY birth_date DESC LIMIT 1;
+--3
+SELECT first_name, last_name, birth_date FROM students WHERE ID BETWEEN 3 AND 5;
+
+
+
+
+
+
